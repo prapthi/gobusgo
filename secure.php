6196d4f431473b575b25a8aa4855761735e1d076
prapthi <naveen@webbylogic.ccom>
2014-04-08 14:32:53 +0530

    Update secure.php

--- secure.php
+++ secure.php
@@ -1,3 +1,27 @@
+<?php
+
+function object_2_array($_POST)
+{
+  print_r($_POST);
+    $array = array();
+    foreach ($_POST as $key=>$value)
+    {
+        if (is_object($value))
+        {
+            $array[$key]=object_2_array($value);
+        }
+        elseif (is_array($value))
+        {
+            $array[$key]=object_2_array($value);
+        }
+        else
+        {
+            $array[$key]=$value;
+        }
+    }
+    return $array;
+}
+?>
 
 
 <div class="section bookno">
