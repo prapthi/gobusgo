7dabfd83ff92d3132c084a27fac48178cecbd175
prapthi <naveen@webbylogic.ccom>
2014-04-08 14:21:47 +0530

    Update secure.php

--- secure.php
+++ secure.php
@@ -6,7 +6,138 @@ include('username.php');
 ob_start();
 session_start();
 ?>
+<?php
+$mode= "TEST";
+$accountId= "5880";
+$amount = $_POST['netprice'];	
+$reference_no= "45454545";
+$return_url= "http://www.gobusgo.in/response.php?DR=GBG";
 
+$hash = "ebskey"."|".$accountId."|".$amount."|".$reference_no."|".$return_url."|".$mode;
+
+print "<pre>";
+print_r($_POST);
+print "</pre>";
+
+//$hash = "4298621700f925bb4e7ea46cee128f12"."|"."14384"."|"."480"."|"."45454545"."|"."http://localhost/gobusgo/response.php?DR=J4"."|".$mode;
+
+$secure_hash = md5($hash);
+
+$name= $_POST['contactname'];
+$address= $_POST['address'];
+$country= $_POST['country'];
+$state= $_POST['state'];
+$city= $_POST['city'];
+$code = $_POST['pincode'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+//$scheduleId= $_POST['scheduleId'];
+
+$description= "Seats";
+$submit = $_POST['continuebooking'];
+
+if($submit= "Continue Booking"){
+$originid = $_SESSION['originid'];
+$destiid = $_SESSION['destiid'];
+$scheduleId = $_SESSION['scheduleId'];
+$depart = $_SESSION['depart'];
+$repdate = str_replace('/', '-', $depart);
+$joudate =date('Y-m-d', strtotime($repdate));
+
+$boardid= $_POST['boardid'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+$city= $_POST['city'];
+$addr= $_POST['address'];
+$address = addslashes($addr);
+$state= $_POST['state'];
+$country= $_POST['country'];
+$contactname= $_POST['contactname'];
+$origname= $_POST['origname'];
+$destiname= $_POST['destiname'];
+$provider= $_POST['provider'];
+$type= $_POST['type'];
+$boardame= $_POST['boardame'];
+
+$seatsNbr= $_POST['seats'];
+
+$totalSeats= $_POST['totalSeats'];
+$netprice= $_POST['netprice'];
+
+$pincode = $_POST['pincode'];
+$netprice= $_POST['netprice'];
+$TotalSeatPrice= $_POST['TotalSeatPrice'];
+$Bookingstatus= "Booked";
+$tbl_name="gobusgo_passdetails";
+$gobusgo = "GB";
+$rand = mt_rand(10000000,999999999);
+$cust_book_id = $gobusgo .$rand;
+
+function object_2_array($_POST)
+{
+    $array = array();
+    foreach ($_POST as $key=>$value)
+    {
+        if (is_object($value))
+        {
+            $array[$key]=object_2_array($value);
+        }
+        elseif (is_array($value))
+        {
+            $array[$key]=object_2_array($value);
+        }
+        else
+        {
+            $array[$key]=$value;
+        }
+    }
+    return $array;
+}
+$detlist =object_2_array($_POST);
+
+for($i=0;$i<$totalSeats;$i++){
+$data_array['passname'] = $detlist['passname'][$i];
+$data_array['age'] = $detlist['age'][$i];
+$data_array['gender'] = $detlist['gender'][$i];
+$data_array['seats'] = $detlist['seats'][$i];
+$details = array_slice($data_array,0);	
+$encodevalue= json_encode($details);
+$list[] = $encodevalue;
+}
+$pass_name= implode($list, ',');
+$parsed = json_decode('['.$pass_name.']');
+
+$passdetails= new stdClass;
+foreach($parsed as $value){
+    $passdetails= new stdClass;
+$name=$value->passname ;
+$age=$value->age;
+$gender=$value->gender;
+$seats=$value->seats;
+$passdetails->age=$age;
+$passdetails->sex=$gender;
+$passdetails->name=$name;
+$passdetails->extraSeatFlagNotFound=true;
+$passdetails->seatNbr=$seats;
+$passDetailsArray[] = $passdetails;
+}
+
+$blockseats=$client->blockSeatsForBooking($username,$password,$scheduleId,$depart,$originid,$destiid,$boardid,$email,$phone,$address,$passDetailsArray);
+print_r(blockseats);
+echo $bookingId= $blockseats->bookingId;
+$_SESSION['bookingId']=$bookingId;
+$cancellationDescList= $blockseats->cancellationDescList;
+$expireTime= $blockseats->expireTime;
+$status= $blockseats->status;
+echo $failCode= $status->code;
+if($failCode=='200' && $bookingId){	
+$result = mysql_query("INSERT INTO $tbl_name(cust_book_id,contact_name,pass_name,address,country, state ,city, pin_code,mobile,email,fromStation,
+toStation,journey_date, scheduleId, provider,bus_type,boarding_name, bookingId,noOfSeats, netprice, totalFare, Bookingstatus)VALUES('$cust_book_id', '$contactname', '$pass_name', '$address', '$country', '$state' ,'$city', '$pincode', '$phone', '$email', '$origname', '$destiname','$joudate', '$scheduleId', '$provider', '$type', '$boardame', '$bookingId', '$totalSeats', '$netprice','$TotalSeatPrice' , '$Bookingstatus')");
+}else{
+header("Location:getTripListV2.php");
+}
+}
+?>
 
 <div class="section bookno">
 
