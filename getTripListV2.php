bc3b5bd117c308f3c02888d9f694a1bdabfea389
prapthi <naveen@webbylogic.ccom>
2014-04-08 17:13:27 +0530

    Update getTripListV2.php

--- getTripListV2.php
+++ getTripListV2.php
@@ -345,56 +345,6 @@ $(function()
 </script>
 
 
-<script type="text/javascript">
-function modify(id)
-{		
-var curr_open='';
-if(document.getElementById('curr_open') != null)
-{
-	curr_open=document.getElementById('curr_open').value;
-}
-	   
-if(curr_open != '' && curr_open != id)
-{
-	if(document.getElementById(curr_open) != null)
-	{
-	var subNavDiv = document.getElementById(id);
-	for (var i=1; i<=3; i++)
-	{
-		if(subNavDiv != i)
-		{
-		var x= i;
-		document.getElementById(x).style.display='none';
-		}
-	}
-	document.getElementById(curr_open).style.display='none';
-	}
-}
-	   
-if(document.getElementById(id) != null)
-{
-	if(document.getElementById(id).style.display == 'none')
-	{
-	var subNavDiv = document.getElementById(id);
-	for (var i=1; i<=3; i++)
-	{
-		if(subNavDiv != i)
-		{
-		var x=i;
-		document.getElementById(x).style.display='none';
-		}
-	}
-	document.getElementById(id).style.display='block';
-	}
-	else
-	document.getElementById(id).style.display='none';
-			
-	document.getElementById('curr_open').value=id;
-}
-}
-
-</script>
-
 
 <?php
 include('footer.php');
