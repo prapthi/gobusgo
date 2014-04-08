7fb6ad77c06232dd593bfcadd09a45b7c3d5bf31
prapthi <naveen@webbylogic.ccom>
2014-04-08 17:28:40 +0530

    Update getTripListV2.php

--- getTripListV2.php
+++ getTripListV2.php
@@ -23,8 +23,8 @@ session_start();
 
 <script src="js/datepicker/jquery.min.js"></script>
 <script src="js/datepicker/jquery-ui.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
-</script>
+<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
+</script> -->
 <script>
 $(document).ready(function(){
   $("#msearch").click(function(){
