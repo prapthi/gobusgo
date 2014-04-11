f204fc352630fbdc8cc7be1c3cff01b4cad90386
prapthi <naveen@webbylogic.ccom>
2014-04-11 12:13:55 +0530

    Update secure.php

--- secure.php
+++ secure.php
@@ -10,13 +10,15 @@ include('username.php');
 ?>
 
 <?php
-echo $mode= "TEST"."<br>";
-echo $accountId= "5880"."<br>";
-echo $amount = '480';	
-echo $reference_no= "45454545"."<br>";
-echo $return_url= "http://www.gobusgo.in/response.php?DR=J4"."<br>";
-echo $hash = "ebskey"."|".$accountId."|".$amount."|".$reference_no."|".$return_url."|".$mode."<br>";
-echo $secure_hash = md5($hash)."<br>";
+$mode= "TEST";
+$accountId= "5880";
+$amount = "480";	
+$reference_no= "45454545";
+$return_url= "http://localhost/gobusgo/response.php?DR=J4";
+
+$hash = "ebskey"."|".$accountId."|".$amount."|".$reference_no."|".$return_url."|".$mode;
+$secure_hash = md5($hash);
+
 
 $name= $_POST['contactname'];
 $address= $_POST['address'];
@@ -140,25 +142,28 @@ toStation,journey_date, scheduleId, provider,bus_type,boarding_name, bookingId,n
 
 <div class="mailcon"> Check your mail for your booking Id sent by us </div>
 <h1>Please Confirm Your Ticket</h1>
-<form method="post" action="https://secure.ebs.in/pg/ma/sale/pay" name="frmTransaction" id="frmTransaction" onSubmit="return validate()">
-<input name="account_id" type="hidden" value="<?php echo $accountId; ?>">
-<input name="return_url" type="hidden" size="60" value="http://www.gobusgo.in/response.php?DR=J4" />
-<input name="mode" type="hidden" size="60" value="<?php echo $mode; ?>" />
-<input name="reference_no" type="hidden" value="45454545" />
-<input name="amount" type="hidden" value="480" />
-<input name="description" type="hidden" value="<?php echo $description; ?>" />
-<input name="name" type="hidden" maxlength="255" value="<?php echo $name; ?>" />
-<input name="address" type="hidden" maxlength="255" value="<?php echo $address; ?>" />
-<input name="city" type="hidden" maxlength="255" value="<?php echo $city; ?>" />
-<input name="state" type="hidden" maxlength="255" value="<?php echo $state; ?>" />
-<input name="postal_code" type="hidden" maxlength="255" value="<?php echo $code; ?>" />
-<input name="country" type="hidden" maxlength="255" value="<?php echo $country; ?>" />
-<input name="phone" type="hidden" maxlength="255" value="<?php echo $phone; ?>" />
-<input name="email" type="hidden" size="60" value="<?php echo $email; ?>" />
+<form  method="post" action="https://secure.ebs.in/pg/ma/sale/pay" name="frmTransaction" id="frmTransaction" onSubmit="return validate()">
+	<input name="account_id" type="hidden" value="<? echo $accountId ?>">
+	
+	<input name="return_url" type="hidden" size="60" value="http://localhost/gobusgo/response.php?DR=J4" />
+	<input name="mode" type="hidden" size="60" value="<? echo $mode?>" />
+	<input name="reference_no" type="hidden" value="45454545" />
+	<input name="amount" type="hidden" value="480" />
+	<input name="description" type="hidden" value="<? echo $description ?>" /> 
+	<input name="name" type="hidden" maxlength="255" value="<? echo $name ?>" />
+	<input name="address" type="hidden" maxlength="255" value="<? echo $address ?>" />
+	<input name="city" type="hidden" maxlength="255" value="<? echo $city ?>" />
+	<input name="state" type="hidden" maxlength="255" value="<? echo $state ?>" />
+	<input name="postal_code" type="hidden" maxlength="255" value="<? echo $code ?>" />
+	<input name="country" type="hidden" maxlength="255" value="<? echo $country ?>" />
+	<input name="phone" type="hidden" maxlength="255" value="<? echo $phone ?>" />
+	<input name="email" type="hidden" size="60" value="<? echo $email?>" />
 
-<input name="secure_hash" type="hidden" size="60" value="<?php echo $secure_hash; ?>" />
-<input name="submitted" value="Submit" type="submit" />
+	<input name="secure_hash" type="hidden" size="60" value="<? echo $secure_hash;?>" />
+	<input name="submitted" value="Submit" type="submit" />
+ 
 </form>
+
 </div>
 
 <script>
