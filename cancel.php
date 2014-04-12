6c25f1fb67277bc5e525b920243e14fc4186b033
prapthi <naveen@webbylogic.ccom>
2014-04-12 09:56:34 +0530

    Update cancel.php

--- cancel.php
+++ cancel.php
@@ -2,18 +2,46 @@
 include('header.php');
 include('username.php');
 ?>
-<?php ob_start();
-session_start();
+
+<script language="javascript">
+function confirmSubmission(){
+	if (confirm("Are you sure you want to submit this information?")){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+</script>
+<?php
+function error(){
+$errors = array (
+	1 => "PNR No is not a valid number!",
+	2 => "Not a valid"
+);
+$error_id = $_GET['err'];
+
+if (isset($_GET['err']) && $_GET['err'] == 'foo') {
+	return $errors[$error_id];
+}else{
+	return $errors[$error_id];
+}
+}
 ?>
 
 <div class="section">
+<div style="color:#FF0000;"><?php echo error(); ?></div>
 	<div class="print">
-		<form action="cancelprocess.php" name="cancelform" id="cancelform" method="post">
+
+	
+		<form name="cancelform" id="cancelform" method="post" action="cancelprocess.php" onsubmit="return confirmSubmission();">
 			<div class="pitem"><img src="images/.png" alt="" required=""></div>
-			<div class="pitem">PNR No. or Ticket No.<input type="text" name="bookingId" value="" required=""></div>
-			<div class="pitem">Email<input type="email" name="pmail" value="" required=""></div>
-			<div class="pitem">Phone<input type="tel" name="ptel" value="" required=""></div>
-			<div class="pclick"><input type="Submit" name="submit" value="Submit" id="submit"></div>
+			<div class="pitem">PNR No. or Ticket No.<input type="text" id="bookingId" name="bookingId" value="" required=""></div>
+			<div class="pitem">Email<input type="email" name="pmail" id="pmail" value="" required=""></div>
+			<div class="pitem">Phone<input type="tel" name="ptel" id="ptel"  value="" required=""></div>
+			<div class="pclick"><input type="Submit" name="submit" value="Submit" id="submit">
+			<input type="hidden" name="confirm_delete" id="confirm_delete" value="0" >
+			</div>
 		</form>
 	</div>
 </div>
