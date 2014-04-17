da4c7560ff02d4aa74ef2a6125e9b446ef091648
prapthi <naveen@webbylogic.ccom>
2014-04-17 14:32:17 +0530

    Update username.php

--- username.php
+++ username.php
@@ -1,10 +1,10 @@
 <?php
-$client = new SoapClient("http://www.ticketgoose.com/bookbustickets/services/TGSWS?wsdl"); 
+/*$client = new SoapClient("http://www.ticketgoose.com/bookbustickets/services/TGSWS?wsdl"); 
 $username = "mrassociates";
- $password= "gobusbo963";
+ $password= "gobusbo963";*/
 
-//$client = new SoapClient("http://dev.ticketgoose.com/bookbustickets/services/TGSWS?wsdl"); 
-//$username = "javaapitest";
-//$password= "testing";
+$client = new SoapClient("http://dev.ticketgoose.com/bookbustickets/services/TGSWS?wsdl"); 
+$username = "javaapitest";
+$password= "testing";
 
 ?>
