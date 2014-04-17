71ad7870fb422067212d5d0cea50450997ed5c55
prapthi <naveen@webbylogic.ccom>
2014-04-17 15:35:38 +0530

    Update response.php

--- response.php
+++ response.php
@@ -33,5 +33,58 @@ if(isset($_GET['DR'])) {
 	$Amount = $response['Amount'];
 if($ResponseMessage == 'Transaction Successful' && $PaymentID!=''){
 $query = mysql_query("UPDATE gobusgo_passdetails set payment_id='$PaymentID', payment_status='Paid', merchant_ref_no='$MerchantRefNo'  WHERE bookingId = '$bookingId'");
+
+$selectqry    = "SELECT * FROM gobusgo_passdetails WHERE bookingId = '$bookingId' ";
+	$sql = mysql_query($selectqry) or die(mysql_error());
+	$fetchseat = mysql_fetch_assoc($sql);
+	
+	
+	$name = $fetchseat['contact_name'] ;
+	$pass_name = $fetchseat['pass_name'] ;
+	$age = $fetchseat['age'] ;
+	$gender = $fetchseat['gender'] ;
+	$address = $fetchseat['address'] ;
+	$country = $fetchseat['country'] ;
+	$state = $fetchseat['state'] ;
+	$city = $fetchseat['city'] ;
+	$pincode = $fetchseat['pin_code'] ;
+	$mobile = $fetchseat['mobile'] ;
+	$email = $fetchseat['email'] ;
+	$cust_book_id = $fetchseat['cust_book_id'] ;
+	
+	$noOfSeats = $fetchseat['noOfSeats'] ;
+	$netprice = $fetchseat['netprice'] ;
+	$totalFare = $fetchseat['totalFare'] ;
+	
+	$fromStation = $fetchseat['fromStation'] ;
+	$toStation = $fetchseat['toStation'] ;
+	$journeyDate = $fetchseat['journey_date'] ;
+	$provider = $fetchseat['provider'] ;
+	$bus_type = $fetchseat['bus_type'] ;
+	$boarding_name = $fetchseat['boarding_name'] ;
+	$seatsbooked = $fetchseat['seatNbr'] ;
+	$bookingId = $fetchseat['bookingId'] ;
+	
+	$bookTickets= $client->BookTicket($username,$password,$bookingId);
+	$cancellist = $bookTickets->cancellationDescList;
+	$extraSeatList = $bookTickets->extraSeatInfoList;
+	
+	foreach($extraSeatList as $extrainfo){
+		$extraSeatInfo =$extrainfo->extraSeatInfo;
+		$seatNbr       =$extrainfo->seatNbr;
+	}	
+		
+		
+	$seat= $status->seatNbr;
+	$travelsPhoneNbr = $bookTickets->travelsPhoneNbr;
+	
+	$status = $bookTickets->status;
+	$code= $status->code;
+		if($status== 'Success'){
+			$qry = mysql_query("UPDATE gobusgo_passdetails SET ticket_status= $status  WHERE bookingId = '$bookingId'");
+		}
+
+
+
 }	
 ?>
